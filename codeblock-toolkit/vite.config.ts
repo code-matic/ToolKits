d7494c6d7503a16b291d39e491b16d6018ca550c
Ayomide <adeshinaayomide12@yahoo.com>
2024-09-23 23:47:53 +0100

    Testing vite config file

--- codeblock-toolkit/vite.config.ts
+++ codeblock-toolkit/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 // import tailwindcss from 'tailwindcss'
 
@@ -11,18 +11,18 @@ import react from '@vitejs/plugin-react';
 //   }
 // })
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd());
-  const PORT = parseInt(env.PORT) || 8080;
+  const PORT = `${env.PORT ?? "8080"}`;
   console.log(env.PORT);
   return {
     plugins: [react()],
     preview: {
-      port: PORT,
+      port: Number(PORT),
       strictPort: true,
     },
     server: {
-      port: PORT,
+      port: Number(PORT),
       host: true,
       origin: `http://0.0.0.0:${PORT}`,
     },
