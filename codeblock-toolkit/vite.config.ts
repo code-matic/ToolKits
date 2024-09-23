8c0c5b9d0ff3678b7391715ea8bc37c8dc4d6568
Ayomide <adeshinaayomide12@yahoo.com>
2024-09-23 23:29:58 +0100

    Fix env port setting

--- codeblock-toolkit/vite.config.ts
+++ codeblock-toolkit/vite.config.ts
@@ -1,12 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+// import tailwindcss from 'tailwindcss'
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   }
+// })
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const PORT = parseInt(env.PORT) || 8080;
+  console.log(env.PORT);
+  return {
+    plugins: [react()],
+    preview: {
+      port: PORT,
+      strictPort: true,
     },
-  }
-})
+    server: {
+      port: PORT,
+      host: true,
+      origin: `http://0.0.0.0:${PORT}`,
+    },
+  };
+});
