f461aa86d763efbaad9a308486aa28233de61e5c
Ayomide <adeshinaayomide12@yahoo.com>
2024-09-24 12:01:57 +0100

    Fixes

--- codeblock-toolkit/vite.config.ts
+++ codeblock-toolkit/vite.config.ts
@@ -1,30 +1,27 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-// import tailwindcss from 'tailwindcss'
+import tailwindcss from 'tailwindcss'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: {
-//       plugins: [tailwindcss()],
-//     },
-//   }
-// })
-
-export default defineConfig(({ mode }) => {
+ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   const PORT = `${env.PORT ?? "8080"}`;
   console.log(env.PORT);
   return {
-    plugins: [react()],
-    preview: {
+   plugins: [react()],
+   css: {
+     postcss: {
+       plugins: [tailwindcss()],
+     },
+     preview: {
       port: PORT,
       strictPort: true,
     },
-    server: {
+     server: {
       port: PORT,
       host: true,
       origin: `http://0.0.0.0:${PORT}`,
     },
-  };
-});
+   }
+  }
+ })
+
